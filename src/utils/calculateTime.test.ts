46d930feac43da9a28edac61fb7fd62093519f19
Felipe Lisboa <lisboa@alunos.utfpr.edu.br>
2023-05-12 14:28:28 -0300

    feat: calculating the time from path

--- src/utils/calculateTime.test.ts
+++ src/utils/calculateTime.test.ts
@@ -3,15 +3,20 @@ import * as ToBeTested from "./calculateTime";
 
 describe("Calculate time algorihtm", () =>{
     test("Calculate the time from a calculateStep", async() =>{
-        const test = await ToBeTested.calculateStep(["A1", "B2"]);
+        const test = await ToBeTested.calculateStep("A1","B1");
+        console.log(test)
+        expect(test).toBe(10.46);
     });
 
-    test("Calculate a time from a entire path", () =>{
-        
+    test("Calculate the time from a calculateStep (undefined)", async() =>{
+        const test2 = await ToBeTested.calculateStep("A1","B2");
+        console.log(test2)
+        expect(test2).toBeUndefined();
     });
 
-    test("Check if there's invalid route", () =>{
-        const response = ToBeTested.findRoute("A91", "B2" , "H7");
-        expect(response.length).toBe(0);
-    }); 
+    test("Calculate a time from a entire path", async() =>{
+        const test = await ToBeTested.calculateTime(["A1","B1"]);
+        expect(test).toBe(10.46);
+    });
+  
 })
--- src/utils/calculateTime.ts
+++ src/utils/calculateTime.ts
@@ -1,13 +1,28 @@
-export async function calculateTime(path: string[]){
-    await Promise.all((path.map(cell =>{
-        // call 
+import axios from 'axios';
 
-        
-    })))
+export async function calculateTime(path: string[]){
+    let totalTime = 0;
+
+    for (let i = 0; i < path.length - 1; i++) {
+        const currentValue = path[i];
+        const nextValue = path[i + 1];
+        const result = await calculateStep(currentValue, nextValue);
+        totalTime+=result;
+        console.log(`Result for ${currentValue} and ${nextValue}: ${result}`);
+      }
+      return totalTime;
 }
 
-export function calculateStep(origin:string,destination:string){
+export async function calculateStep(origin:string,destination:string){
 
-    
-    return
+    const {data} = await axios.get("https://mocki.io/v1/10404696-fd43-4481-a7ed-f9369073252f")
+    // find the current cell
+
+
+
+    console.log(data[origin][destination])
+
+
+    // find the time to the destination cell
+    return data[origin][destination];
 }
